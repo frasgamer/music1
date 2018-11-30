49945bf87d64298d0744ad6d45123fd5785c8c21
frasgamer <44925264+frasgamer@users.noreply.github.com>
2018-11-30 20:21:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,25 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+
+const Util = require('discord.js');
+
+const getYoutubeID = require('get-youtube-id');
+
+const fetchVideoInfo = require('youtube-info');
+
+const YouTube = require('simple-youtube-api');
+
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+
+const queue = new Map();
+
+const ytdl = require('ytdl-core');
+
+const fs = require('fs');
+
+const gif = require("gif-search");
+
+const client = new Discord.Client({disableEveryone: true});
+
 var prefix = "-";
 
 client.on('ready', () => {
